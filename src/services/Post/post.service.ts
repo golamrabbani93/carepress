79002a5dd4f0586b4ff1ba14b24a5f425cec33af
Golam Rabbani <golamrabbani9373@gmail.com>
2024-09-30 21:42:02 +0600

    create post service

--- src/services/Post/post.service.ts
+++ src/services/Post/post.service.ts
@@ -1,6 +1,26 @@
 'use server';
 
 import envConfig from '@/config/envConfig';
+import axiosInstance from '@/lib/AxiosInstance';
+import {revalidateTag} from 'next/cache';
+
+// *create post
+export const createPost = async (formData: FormData): Promise<any> => {
+	try {
+		const {data} = await axiosInstance.post('/posts/create-post', formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data',
+			},
+		});
+
+		revalidateTag('posts');
+
+		return data;
+	} catch (error) {
+		console.log(error);
+		throw new Error('Failed to create post');
+	}
+};
 
 //*get Post Data
 export const getAllPosts = async () => {
