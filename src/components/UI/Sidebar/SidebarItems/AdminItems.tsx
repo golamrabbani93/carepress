21446c3a5a51e69c9172f6a01d24df996a319d69
Golam Rabbani <golamrabbani9373@gmail.com>
2024-10-03 21:16:30 +0600

    sidebar items complete for admin

--- src/components/UI/Sidebar/SidebarItems/AdminItems.tsx
+++ src/components/UI/Sidebar/SidebarItems/AdminItems.tsx
@@ -1,11 +1,10 @@
-//* Sidebar menu items
-
-import {Home, Package, ClipboardList, User} from 'lucide-react';
+//* Sidebar menu items for admin users
+import {Home, Users, ScrollText, TableOfContents, CircleDollarSign} from 'lucide-react';
 
 export const ADMINITEMS = [
-	{name: 'Dashboard', icon: <Home />},
-	{name: 'Manage Users', icon: <User />},
-	{name: 'Manage Orders', icon: <ClipboardList />},
-	{name: 'Products', icon: <Package />},
-	{name: 'Reports', icon: <ClipboardList />},
+	{name: 'Dashboard', icon: <Home />, path: '/dashboard/admin'},
+	{name: 'Manage Users', icon: <Users />, path: '/dashboard/admin/users'},
+	{name: 'Manage Posts', icon: <ScrollText />, path: '/dashboard/admin/posts'},
+	{name: 'Manage Content', icon: <TableOfContents />, path: '/dashboard/admin/content'},
+	{name: 'Payment History', icon: <CircleDollarSign />, path: '/dashboard/admin/payments'},
 ];
