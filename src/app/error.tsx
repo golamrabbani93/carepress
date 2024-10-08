89887d923d96df6452f28b9c84ab6a49cba17abd
Golam Rabbani <golamrabbani9373@gmail.com>
2024-10-08 20:54:31 +0600

    design error page and final deploy

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,31 +1,27 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
+import {useEffect} from 'react';
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error;
-  reset: () => void;
-}) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    /* eslint-disable no-console */
-    console.error(error);
-  }, [error]);
+export default function Error({error, reset}: {error: Error; reset: () => void}) {
+	useEffect(() => {
+		// Log the error to an error reporting service
+		console.error(error);
+	}, [error]);
 
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center min-h-screen p-4">
+			<div className="p-6 rounded-lg shadow-md text-center max-w-sm w-full">
+				<h2 className="text-xl sm:text-2xl font-bold text-red-600 mb-4">Something went wrong!</h2>
+				<p className="text-sm sm:text-base  mb-6">
+					We&apos;re sorry for the inconvenience. Please try again.
+				</p>
+				<button
+					onClick={() => reset()}
+					className="bg-primary text-white px-4 py-2 rounded hover:bg-red-400 transition duration-200"
+				>
+					Try Again
+				</button>
+			</div>
+		</div>
+	);
 }
